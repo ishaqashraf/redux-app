0c520fc583e91ba035a617d2f7a6220cb527123c
Ishaq Ashraf <ishaqashraf@Ishaqs-iMac.local>
2018-07-05 21:23:26 +0500

    store and provide

---
+++ src/components/courses/courses.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+
+class Courses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: { title: "" }
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Courses</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
+      </div>
+    );
+  }
+}
+
+export default Courses;
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Courses from './courses/courses';
+
+
+class Main extends Component {
+    render() {
+        return (
+            <Courses />
+        );
+    }
+}
+
+
+export default Main;
